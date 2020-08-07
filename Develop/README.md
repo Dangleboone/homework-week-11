1a5bcee0a64e311752f4b77bd0a65ef74fe1eba4
Daniel Altman <Daniel.Altman4@gmail.com>
2020-08-06 21:01:33 -0700

    getting help from group to fix program

---
+++ Develop/README.md
@@ -0,0 +1,14 @@
+
+# Title: readme generator
+## Author: daniel
+## Github Link: [Github User Profile](https://github.com/dangleboone)
+### License: ![Github License](https://img.shields.io/badge/license-MIT-blue.svg)
+#### Table of Contents: 
+Instructions | Description 
+--- | ---
+Installation | npm i
+Testing | npm test
+Usage | open source
+### Project Description:  
+program that creates readme
+###### If you have any questions email me daniel.altman4@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,112 +1,82 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
-
-// array of questions for user
+var inquirer = require("inquirer")
+var fs = require("fs")
+// array of questions for user to input
 const questions = [
-  {
+{
     type: "input",
-    name: "title",
-    message: "Please enter your project title",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please enter a description of the project",
-  },
-  {
-    type: "input",
-    name: "installDetails",
-    message: "Please enter the installation instructions",
-  },
-  {
-    type: "input",
-    name: "usageInformation",
-    message: "Please enter the usage information",
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message: "Please enter the contributing information",
-  },
-  {
-    type: "input",
-    name: "testInformation",
-    message: "Please enter test information",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Choose a license for your project",
-    choices: ["MIT License", "Apache License", "The Unlicense", "GNU AGPLv3"],
-    filter: function (val) {
-      return val.toLowerCase();
-    },
-  },
-  {
-    type: "input",
-    name: "username",
-    message: "Please enter your GitHub username",
-  },
-  {
+    name: "github",
+    message: "github username"
+},
+{
     type: "input",
     name: "email",
-    message: "Please enter your email address",
-  },
+    message: "email address"
+},
+{
+    type: "input",
+    name: "title",
+    message: "project title"
+},
+{
+    type: "input",
+    name: "description",
+    message: "project description"
+},
+{
+    type: "list",
+    name: "license",
+    message: "enter license",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+},
+{
+    type: "input",
+    name: "tests",
+    message: "enter testing information",
+    default: "npm test"
+},
+{
+    type: "input",
+    name: "install",
+    message: "enter installation information",
+    default: "npm i"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "enter usage information"
+},
+{
+    type: "input",
+    name: "contributors",
+    message: "enter contributors information",
+    default: "Daniel"
+},
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-  let readMe = `
-  # ${data.title}
-  ### Description
-  ${data.description}
-  
-  
-  ### Installation
-  
-  ${data.installDetails}
-  ### Usage
-  ${data.usageInformation}
-  
-  ### Contributing
-  
-  ${data.contribution}
-  ### Tests
-  
-  ${data.testInformation}
-  
-  ## Author
-  
-  Github Repository - [${data.username}](https://github.com/${data.username})
-  Email - ${data.email}
-  
-  ## License
-  
-  This project is licensed under the ${data.license}
-  `;
-
-  fs.writeFile(`${fileName}.md`, readMe, function (err) {
-    if (err) throw err;
-    console.log("File has been saved to your current project directory!");
-  });
-}
-
-// function to initialize program
+// function that initializes program
 function init() {
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      writeToFile("README", answers);
-      // Use user feedback for... whatever!!
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        console.log(error);
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else when wrong
-      }
-    });
+inquirer.prompt(questions).then(function(tv){
+console.log(tv)
+var readmeText = `
+# Title: ${tv.title}
+## Author: ${tv.contributors}
+## Github Link: [Github User Profile](https://github.com/${tv.github})
+### License: ![Github License](https://img.shields.io/badge/license-${tv.license}-blue.svg)
+#### Table of Contents: 
+Instructions | Description 
+--- | ---
+Installation | ${tv.install}
+Testing | ${tv.tests}
+Usage | ${tv.usage}
+### Project Description:  
+${tv.description}
+###### If you have any questions email me ${tv.email}
+`
+fs.writeFileSync("README.md", readmeText, function(){
+    console.log("filegenerator", readmeText)
+})
+
+})
 }
 
 // function call to initialize program
